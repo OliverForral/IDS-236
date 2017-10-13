4371eec3c07e0c10bc7718d94df758a7aa88c05c
Oliver Forral <Oliver.Forral@gmail.com>
2017-10-13 11:05:33 -0700

    Made some options unselectable

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
 import React, { Component } from 'react';
+import { Select } from 'antd';
 import logo from './logo.svg';
+import 'antd/dist/antd.css';
 import './App.css';
+
+const Option = Select.Option;
+
+const children = [];
+for (let i = 10; i < 36; i++) {
+  const sqrt = Math.sqrt(i);
+  if (Math.round(sqrt) === sqrt) {
+    children.push(<Option key={i.toString(36) + i} disabled={true}>{i.toString(36) + i}</Option>)
+  } else {
+    children.push(<Option key={i.toString(36) + i}>{i.toString(36) + i}</Option>);
+  }
+}
+
+function handleChange(value) {
+  console.log(`selected ${value}`);
+}
 
 class App extends Component {
   render() {
@@ -13,6 +31,15 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Select
+          mode="multiple"
+          style={{ width: '100%' }}
+          placeholder="Please select"
+          defaultValue={['g16', 'p25']}
+          onChange={handleChange}
+        >
+          {children}
+        </Select>
       </div>
     );
   }
